b56d0b6421049d464170f4e93d7ad86be70400ca
Natasha Rydell <natasha@briljantkommunikation.se>
2023-09-28 14:01:57 +0200

    fix: error

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,7 +12,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // Animal symbols
     let symbols = ["fa-solid fa-hippo", "fa-solid fa-dog", "fa-solid fa-otter", "fa-solid fa-cow", "fa-solid fa-fish", "fa-solid fa-dragon","fa-solid fa-kiwi-bird", "fa-solid fa-worm", "fa-solid fa-spider", "fa-solid fa-shrimp", "fa-solid fa-mosquito", "fa-solid fa-locust", "fa-solid fa-horse-head", "fa-solid fa-horse","fa-solid fa-frog", "fa-solid fa-dove", "fa-solid fa-crow", "fa-solid fa-cat","fa-solid fa-bugs"];
 
-    /** Double the symbols for pair and shuffles them
+}); 
+
+/** 
+ *     Double the symbols for pair and shuffles them
     symbols = shuffle([...symbols, ...symbols]);
 
     Assign symbols to cards
@@ -22,4 +25,4 @@ document.addEventListener('DOMContentLoaded', function() {
         icon.className = symbols[index];
         symbolElement.appendChild(icon);
     });
-} */
+ */
