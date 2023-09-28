3da93e2cad3543f8474c12a24b2c21bec102b6dd
Natasha Rydell <natasha@briljantkommunikation.se>
2023-09-29 00:45:39 +0200

    feat: added neccessary functions + symbols on card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,76 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // --------- Card functions
-    let cards = document.getElementsByClassName('card');
 
-    // Flipping cards
+    // -------- Flipping cards
+    let cards = document.querySelectorAll('.card');
     for (let card of cards) {
         card.addEventListener('click', function() {
             this.classList.toggle('flipped');
         });
     }
 
-    // Animal symbols
+    /**
+     * Adding symbols to the cards with the right amount of pairs based on chosen difficulty. 
+     * The symbols are cloned with spread operator and shuffled with sort method.
+     */
+function generateSymbols(difficulty) {
     let symbols = ["fa-solid fa-hippo", "fa-solid fa-dog", "fa-solid fa-otter", "fa-solid fa-cow", "fa-solid fa-fish", "fa-solid fa-dragon","fa-solid fa-kiwi-bird", "fa-solid fa-worm", "fa-solid fa-spider", "fa-solid fa-shrimp", "fa-solid fa-mosquito", "fa-solid fa-locust", "fa-solid fa-horse-head", "fa-solid fa-horse","fa-solid fa-frog", "fa-solid fa-dove", "fa-solid fa-crow", "fa-solid fa-cat","fa-solid fa-bugs"];
 
-}); 
+    let numbersOfPairs;
+    switch (difficulty) {
+        case 'easy':
+            numbersOfPairs = 1;
+            break;
+        case 'medium':
+            numbersOfPairs = 8;
+            break;
+        case 'hard':
+            numbersOfPairs = 12;
+            break;
+    }
 
-/** 
- *     Double the symbols for pair and shuffles them
-    symbols = shuffle([...symbols, ...symbols]);
+    let selectedSymbols = symbols.slice(0, numbersOfPairs);
 
-    Assign symbols to cards
-    cards.forEach((card , index) => {
-        let symbolElement = card.querySelector('.symbol');
-        let icon = document.createElement('i');
-        icon.className = symbols[index];
-        symbolElement.appendChild(icon);
+    let gameSymbols = [...selectedSymbols, ...selectedSymbols];
+
+    gameSymbols.sort(() => Math.random() - 0.5);
+
+    return gameSymbols;
+}
+    /**
+     * Redirect player to right page with right amount of cards and pairs
+     * based on chosen difficulty.
+     */
+    document.getElementById('start-game').addEventListener('click', function() {
+        let difficulty = document.getElementById('difficulty').value;
+        startGame(difficulty);
     });
- */
+    document.getElementById('start-game').addEventListener('click', function() {
+        let selectedPage = document.getElementById('difficulty').value;
+        window.location.href = selectedPage;
+    });
+        
+        if (window.location.pathname.includes('easy-game.html')) {
+
+        }
+
+});
+
+
+function removePair() {
+
+}
+
+function checkCards() {
+
+}
+
+function choseTheme() {
+
+}
+function countMoves() {
+
+}
+
+function timer() {
+
+}
