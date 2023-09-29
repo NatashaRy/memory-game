f3aa1ef8bff0b00df32c11ab58257c042098af3c
Natasha Rydell <natasha@briljantkommunikation.se>
2023-09-29 14:20:15 +0200

    feat: added function based on diffeculty

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     // -------- Flipping cards
     let cards = document.querySelectorAll('.card');
+
     for (let card of cards) {
         card.addEventListener('click', function() {
             this.classList.toggle('flipped');
@@ -9,53 +10,76 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     /**
+     * Determind difficulty based on page. 
      * Adding symbols to the cards with the right amount of pairs based on chosen difficulty. 
      * The symbols are cloned with spread operator and shuffled with sort method.
      */
-function generateSymbols(difficulty) {
-    let symbols = ["fa-solid fa-hippo", "fa-solid fa-dog", "fa-solid fa-otter", "fa-solid fa-cow", "fa-solid fa-fish", "fa-solid fa-dragon","fa-solid fa-kiwi-bird", "fa-solid fa-worm", "fa-solid fa-spider", "fa-solid fa-shrimp", "fa-solid fa-mosquito", "fa-solid fa-locust", "fa-solid fa-horse-head", "fa-solid fa-horse","fa-solid fa-frog", "fa-solid fa-dove", "fa-solid fa-crow", "fa-solid fa-cat","fa-solid fa-bugs"];
+    function generateSymbols(difficulty) {
+        let symbols = ["fa-solid fa-hippo", "fa-solid fa-dog", "fa-solid fa-otter", "fa-solid fa-cow", "fa-solid fa-fish", "fa-solid fa-dragon","fa-solid fa-kiwi-bird", "fa-solid fa-worm", "fa-solid fa-spider", "fa-solid fa-shrimp", "fa-solid fa-mosquito", "fa-solid fa-locust", "fa-solid fa-horse-head", "fa-solid fa-horse","fa-solid fa-frog", "fa-solid fa-dove", "fa-solid fa-crow", "fa-solid fa-cat","fa-solid fa-bugs"];
+    
+        let numbersOfPairs;
+        switch (difficulty) {
+            case 'easy':
+                numbersOfPairs = 2;
+                break;
+            case 'medium':
+                numbersOfPairs = 8;
+                break;
+            case 'hard':
+                numbersOfPairs = 12;
+                break;
+        }
 
-    let numbersOfPairs;
-    switch (difficulty) {
-        case 'easy':
-            numbersOfPairs = 1;
-            break;
-        case 'medium':
-            numbersOfPairs = 8;
-            break;
-        case 'hard':
-            numbersOfPairs = 12;
-            break;
+        let selectedSymbols = symbols.slice(0, numbersOfPairs);
+        let gameSymbols = [...selectedSymbols, ...selectedSymbols];
+        
+        gameSymbols.sort(() => Math.random() - 0.5);
+        console.log(`Shuffled symbols ${gameSymbols}`);
+        return gameSymbols;
     }
 
-    let selectedSymbols = symbols.slice(0, numbersOfPairs);
+    if (window.location.pathname.includes('easy-game.html')) {
+        console.log('Easy game page loaded.');
 
-    let gameSymbols = [...selectedSymbols, ...selectedSymbols];
+        let shuffledSymbols = generateSymbols('easy');
+        for (let i = 0; i < cards.length; i++) {
+            let symbolElement = cards[i].querySelector('.symbol');
+            let icon = document.createElement('i');
+            icon.className = shuffledSymbols[i];
+            symbolElement.appendChild(icon);
+        }
+    } else if (window.location.pathname.includes('medium-game.html')) {
+        console.log('Medium game page loaded.');
 
-    gameSymbols.sort(() => Math.random() - 0.5);
+        let shuffledSymbols = generateSymbols('medium');
+        for (let i = 0; i < cards.length; i++) {
+            let symbolElement = cards[i].querySelector('.symbol');
+            let icon = document.createElement('i');
+            icon.className = shuffledSymbols[i];
+            symbolElement.appendChild(icon);
+        }
+    } else if (window.location.pathname.includes('hard-game.html')) {
+        console.log('Hard game page loaded.');
 
-    return gameSymbols;
-}
-    /**
-     * Redirect player to right page with right amount of cards and pairs
-     * based on chosen difficulty.
-     */
-    document.getElementById('start-game').addEventListener('click', function() {
-        let difficulty = document.getElementById('difficulty').value;
-        startGame(difficulty);
-    });
-    document.getElementById('start-game').addEventListener('click', function() {
-        let selectedPage = document.getElementById('difficulty').value;
-        window.location.href = selectedPage;
-    });
-        
-        if (window.location.pathname.includes('easy-game.html')) {
-        } else if (window.location.pathname.includes('medium-game.html')) {
+        let shuffledSymbols = generateSymbols('hard');
+        for (let i = 0; i < cards.length; i++) {
+            let symbolElement = cards[i].querySelector('.symbol');
+            let icon = document.createElement('i');
+            icon.className = shuffledSymbols[i];
+            symbolElement.appendChild(icon);
+        }
+    } 
 
-        } else if (window.location.pathname.includes('hard-game.html')) {}
+    let startGameButton = document.getElementById("start-game");
+    if (startGameButton) {
+        startGameButton.addEventListener('click', function() {
+            let difficulty = document.getElementById('difficulty').value;
+            window.location.href = difficulty + '-game.html';
+        });
+    }
+})
 
-});
-
+/**
 
 function removePair() {
 
@@ -65,7 +89,7 @@ function checkCards() {
 
 }
 
-function choseTheme() {
+function chooseTheme() {
 
 }
 function countMoves() {
@@ -74,4 +98,4 @@ function countMoves() {
 
 function timer() {
 
-}
+} */
